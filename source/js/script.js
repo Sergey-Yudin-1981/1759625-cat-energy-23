97e545e9390e738fd294b221e22634ece117622b
Seregy-Yudin-1981 <usv81@mail.ru>
2021-09-06 12:14:20 +0500

    вснес попровки 5-1-3

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+const example = document.querySelector('.example');
+const range = document.querySelector('.toggle-controls__scale');
+const beforeWrap = document.querySelector('.example__pic-wrap--before');
+const afterWrap = document.querySelector('.example__pic-wrap--after');
+
+range.addEventListener('input', function (evt) {
+  let value = range.value;
+
+  afterWrap.style.width = `${value}%`;
+
+  if (window.innerWidth >= 1440) {
+  	example.style.backgroundImage = `linear-gradient(to right, #F2F2F2 ${100 - 0.5*value}%, #EAEAEA ${0.5*value}%)`;
+  	// конечная точка градиента = 50%, поэтому находим значение на единицу 0.5 и считаем от value
+  }
+
+});
